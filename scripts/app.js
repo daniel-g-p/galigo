a4194a9afc48cb19222cd57c12e4bc37edad0446
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-05-10 17:17:15 +0200

    Cleaned up JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,22 @@
+// 1 DOM Elements
+
 const navToggle = document.querySelector(".navToggle");
 const mobileNav = document.querySelector(".mobileNav");
+const navLinks = document.querySelectorAll(".links a, .hero a");
+const problemNavigation = document.querySelectorAll(".imageSwitch div, .imageIndicator div");
+const problemTextblock = document.querySelector(".textBlock.problem");
+const solutionNavigation = document.querySelectorAll(".solutionIcon");
+const solutionTextblock = document.querySelector(".textBlock.solution");
+const checkBoxLabel = document.querySelector(".checkBoxLabel");
+const form = document.querySelector("form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const subjectInput = document.querySelector("#subject");
+const messageInput = document.querySelector("#message");
+const inputs = [nameInput, emailInput, subjectInput, messageInput];
 
-const toggleNav = () => {
-    mobileNav.classList.toggle("active");
-    navToggle.classList.toggle("active");
-    if (mobileNav.classList.contains("active")) {
-        mobileNav.style.maxHeight = mobileNav.scrollHeight + "px";
-    } else {
-        mobileNav.style.maxHeight = 0;
-    }
-}
 
-navToggle.addEventListener("click", () => {
-    toggleNav();
-});
-
-const demoScreensN = document.querySelectorAll(".screens img").length;
-let screenNumber = Number(document.querySelector(".screens .active").classList.value.replace(/[^0-9]/g, ''));
-
-setInterval(() => {
-    const currentScreen = document.querySelector(`.screen${screenNumber}`);
-    screenNumber++;
-    if (screenNumber > demoScreensN) {
-        screenNumber = 1;
-    }
-    const newScreen = document.querySelector(`.screen${screenNumber}`);
-    currentScreen.classList.replace("active", "fadeOut");
-    newScreen.classList.add("active");
-    setTimeout(() => currentScreen.classList.remove("fadeOut"), 1000);
-}, 5000);
+// 2 CONTENT ARRAYS
 
 const problemsContent = [{
         title: "Problem 1",
@@ -60,38 +48,7 @@ const problemsContent = [{
     },
 ];
 
-const totalProblems = problemsContent.length;
-const problemNavigation = document.querySelectorAll(".imageSwitch div, .imageIndicator div");
-const problemTextblock = document.querySelector(".textBlock.problem");
-let problemNumber = Number(document.querySelector(".problemImage.active").classList.value.replace(/[^0-9]/g, ''));
-
-problemNavigation.forEach(button => button.addEventListener("click", () => {
-    const currentElements = document.querySelectorAll(`.problem${problemNumber}`);
-    if (button.classList.contains("next")) {
-        problemNumber++;
-        if (problemNumber > totalProblems) {
-            problemNumber = 1;
-        }
-    } else if (button.classList.contains("previous")) {
-        problemNumber--;
-        if (problemNumber < 1) {
-            problemNumber = 7;
-        }
-    } else {
-        problemNumber = Number(button.classList.value.replace(/[^0-9]/g, ''));
-    }
-    const newElements = document.querySelectorAll(`.problem${problemNumber}`);
-    currentElements.forEach(e => e.classList.remove("active"));
-    newElements.forEach(e => e.classList.add("active"));
-    problemTextblock.classList.add("fadeOut");
-    setTimeout(() => {
-        problemTextblock.firstElementChild.innerText = problemsContent[problemNumber - 1].title;
-        problemTextblock.lastElementChild.innerText = problemsContent[problemNumber - 1].text;
-        problemTextblock.classList.remove("fadeOut");
-    }, 500)
-}));
-
-const servicesContent = [{
+const solutionsContent = [{
         title: "Service 1",
         text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
     },
@@ -121,10 +78,73 @@ const servicesContent = [{
     },
 ];
 
-const solutionNavigation = document.querySelectorAll(".solutionIcon");
-const solutionTextblock = document.querySelector(".textBlock.solution");
-let solutionNumber = Number(document.querySelector(".solutionIcon.active").classList.value.replace(/[^0-9]/g, ''));
 
+// 3 VARIABLES
+const totalScreens = document.querySelectorAll(".screens img").length;
+let screenNumber = Number(document.querySelector(".screens .active").classList.value.replace(/[^0-9]/g, ''));
+const totalProblems = problemsContent.length;
+let problemNumber = Number(document.querySelector(".problemImage.active").classList.value.replace(/[^0-9]/g, ''));
+const totalSolutions = solutionsContent.length;
+let solutionNumber = Number(document.querySelector(".solutionIcon.active").classList.value.replace(/[^0-9]/g, ''));
+const emailFormat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+
+// 4 EVENT LISTENERS
+
+// 4.1 NAVIGATION MENU
+navToggle.addEventListener("click", () => {
+    toggleNav();
+});
+navLinks.forEach(l => l.addEventListener("click", e => {
+    e.preventDefault();
+    mobileNav.classList.remove("active");
+    navToggle.classList.remove("active");
+    mobileNav.style.maxHeight = 0;
+    const section = document.querySelector(`${l.getAttribute("href")}`);
+    setTimeout(() => section.scrollIntoView({ behavior: "smooth" }), 500);
+}));
+
+// 4.2 HERO SECTION
+setInterval(() => {
+    const currentScreen = document.querySelector(`.screen${screenNumber}`);
+    screenNumber++;
+    if (screenNumber > totalScreens) {
+        screenNumber = 1;
+    }
+    const newScreen = document.querySelector(`.screen${screenNumber}`);
+    currentScreen.classList.replace("active", "fadeOut");
+    newScreen.classList.add("active");
+    setTimeout(() => currentScreen.classList.remove("fadeOut"), 1000);
+}, 5000);
+
+// 4.3 PROBLEM SECTION
+problemNavigation.forEach(button => button.addEventListener("click", () => {
+    const currentElements = document.querySelectorAll(`.problem${problemNumber}`);
+    if (button.classList.contains("next")) {
+        problemNumber++;
+        if (problemNumber > totalProblems) {
+            problemNumber = 1;
+        }
+    } else if (button.classList.contains("previous")) {
+        problemNumber--;
+        if (problemNumber < 1) {
+            problemNumber = 7;
+        }
+    } else {
+        problemNumber = Number(button.classList.value.replace(/[^0-9]/g, ''));
+    }
+    const newElements = document.querySelectorAll(`.problem${problemNumber}`);
+    currentElements.forEach(e => e.classList.remove("active"));
+    newElements.forEach(e => e.classList.add("active"));
+    problemTextblock.classList.add("fadeOut");
+    setTimeout(() => {
+        problemTextblock.firstElementChild.innerText = problemsContent[problemNumber - 1].title;
+        problemTextblock.lastElementChild.innerText = problemsContent[problemNumber - 1].text;
+        problemTextblock.classList.remove("fadeOut");
+    }, 500)
+}));
+
+// 4.4 SOLUTIONS SECTION
 solutionNavigation.forEach(button => button.addEventListener("click", () => {
     const currentSolution = document.querySelector(`.solution${solutionNumber}`);
     solutionNumber = Number(button.classList.value.replace(/[^0-9]/g, ''));
@@ -132,26 +152,13 @@ solutionNavigation.forEach(button => button.addEventListener("click", () => {
     button.classList.add("active");
     solutionTextblock.classList.add("fadeOut");
     setTimeout(() => {
-        solutionTextblock.firstElementChild.innerText = servicesContent[solutionNumber - 1].title;
-        solutionTextblock.lastElementChild.innerText = servicesContent[solutionNumber - 1].text;
+        solutionTextblock.firstElementChild.innerText = solutionsContent[solutionNumber - 1].title;
+        solutionTextblock.lastElementChild.innerText = solutionsContent[solutionNumber - 1].text;
         solutionTextblock.classList.remove("fadeOut");
     }, 500)
 }));
 
-const checkBoxLabel = document.querySelector(".checkBoxLabel");
-
-checkBoxLabel.addEventListener("click", () => {
-    checkBoxLabel.classList.toggle("checked");
-});
-
-const form = document.querySelector("form");
-const nameInput = document.querySelector("#name");
-const emailInput = document.querySelector("#email");
-const subjectInput = document.querySelector("#subject");
-const messageInput = document.querySelector("#message");
-const inputs = [nameInput, emailInput, subjectInput, messageInput];
-const emailFormat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
+// 4.5 FORM SECTION
 form.addEventListener("submit", e => {
     e.preventDefault();
     inputs.forEach(i => {
@@ -168,16 +175,19 @@ form.addEventListener("submit", e => {
         form.lastElementChild.innerHTML = "Thank you";
     }
 });
-
+checkBoxLabel.addEventListener("click", () => {
+    checkBoxLabel.classList.toggle("checked");
+});
 inputs.forEach(i => i.addEventListener("input", () => i.parentElement.classList.remove("error")));
 
-const navLinks = document.querySelectorAll(".links a, .hero a");
 
-navLinks.forEach(l => l.addEventListener("click", e => {
-    e.preventDefault();
-    mobileNav.classList.remove("active");
-    navToggle.classList.remove("active");
-    mobileNav.style.maxHeight = 0;
-    const section = document.querySelector(`${l.getAttribute("href")}`);
-    setTimeout(() => section.scrollIntoView({ behavior: "smooth" }), 500);
-}));
+// 5 HELPER FUNCTIONS
+const toggleNav = () => {
+    mobileNav.classList.toggle("active");
+    navToggle.classList.toggle("active");
+    if (mobileNav.classList.contains("active")) {
+        mobileNav.style.maxHeight = mobileNav.scrollHeight + "px";
+    } else {
+        mobileNav.style.maxHeight = 0;
+    }
+}
