8e94bad993f103d4b642261eaf052534a275cc75
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-05-14 13:23:31 +0200

    Optimized images and added content

--- scripts/app.js
+++ scripts/app.js
@@ -23,62 +23,58 @@ const inputs = [nameInput, emailInput, subjectInput, messageInput];
 // 2 CONTENT ARRAYS
 
 const problemsContent = [{
-        title: "Problem 1",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Slow Traffic Updates",
+        text: "One of the main causes for traffic congestion is poor anticipation. As it stands today, by the time traffic congestion is detected and communicated to road users, the situation has more often than not already aggravated."
     },
     {
-        title: "Problem 2",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Undetected Road Anomalies",
+        text: "Roadworks, accidents, and deviations happen to be a part of urban mobility. And while they may sometimes be inevitable, we should certainly strive to manage them with more reactive, efficient, and secure methods."
     },
     {
-        title: "Problem 3",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Mass Traffic Congestion",
+        text: "We've all been stuck in a line of cars that stretches into the horizon at some point. To this day, traffic congestion causes major issues related to mental health, time efficiency, and economic productivity."
     },
     {
-        title: "Problem 4",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Unreliable Public Transport",
+        text: "One of the greatest assets of urban cities is ironically also a major source of frustration. Many of us are dependent on public transport, and its service quality and timeliness still leave a lot to be desired."
     },
     {
-        title: "Problem 5",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+        title: "Inefficient Delivery Services",
+        text: "In an economy where virtually everything can be purchased through a screen, we need to find a better way to get online orders from store to door, for the benefit of consumers, sellers, and carriers alike."
     },
     {
-        title: "Problem 6",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Excess Fuel Consumption",
+        text: "There are only so many natural resources available to us, and we are racing towards a depletion of our raw materials. Urban traffic in particular can't sustain its current fuel consumption for much longer."
     },
     {
-        title: "Problem 7",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Increased Pollution",
+        text: "Our planet is the most valuable thing we have, yet we don't treat it as such. In urban cities more than anywhere else, it's about time that we start making a real effort to reduce our greenhouse gas emissions."
     },
 ];
 
 const solutionsContent = [{
-        title: "Service 1",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Road Anomaly Detection",
+        text: "Using the latest geolocation technology and complex image recognition algorithms, GaliGo automatically detects and locates road anomalies such as holes in the road, accidents, and roadworks."
     },
     {
-        title: "Service 2",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Traffic Signal Analysis",
+        text: "Traffic congestion issues tends to occur repeatedly in the same areas, which is why GaliGo uses its AI-powered technology to identify miscoordination between traffic signals in urban communities along with possible improvements."
     },
     {
-        title: "Service 3",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Smart Itinerary Planning",
+        text: "We all share the same roads to get from point A to point B, and GaliGo takes this into account in a real-time navigation system that considers all road users in order to propose itineraries that them to their destination via the fastest and most efficient route."
     },
     {
-        title: "Service 4",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Real-time Traffic Updates",
+        text: "Unexpected things happen on the road, and while GaliGo can't promise to eliminate them entirely, it sure can offer to identify and communicate them faster and more accurately than it has ever been done before."
     },
     {
-        title: "Service 5",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Live Public Transport Information",
+        text: "GaliGo's technology is, among other things, deployed on public transport vehicles, thus allowing users to know exactly where their next bus is at this very moment and when it will reach them at their stop."
     },
     {
-        title: "Service 6",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
-    },
-    {
-        title: "Service 7",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis placeat totam repellendus voluptate maiores animi distinctio expedita recusandae vero quasi dolorem consequuntur officia quas hic, iusto voluptatem ut rem."
+        title: "Delivery Tracking",
+        text: "With GaliGo, the days of missing deliveries due to unpunctual carriers or impractical schedules are over: Users can now know well in advance when they can expect their online orders to be delivered and can follow them throughout the entire delivery process."
     },
 ];
 
